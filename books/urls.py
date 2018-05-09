658d92af0a590faa5cb2d79bd43995aeb2a1004b
konrad <konrad.szymanski07@gmail.com>
2018-05-09 12:56:46 +0200

    add general template and background image

--- books/urls.py
+++ books/urls.py
@@ -3,6 +3,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = (
-     path('', views.index),
+    path('', views.index),
+    path('test/', views.test),
+    path('test2/', views.test2),
+
 )
 
--- books/views.py
+++ books/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
-from django.template import RequestContext
 from books.models import Plik
-from django.http import HttpResponse
 
 # Create your views here.
 
@@ -10,8 +8,15 @@ from django.http import HttpResponse
 
 
 def index(request):
-    context = RequestContext(request)
     plik = Plik.objects.create(adres="adres", nazwa="pliczek")
     context_dict = {"file": plik}
 
     return render(request, 'home.html', context_dict)
+
+
+def test(request):
+    return render(request, 'test.html')
+
+
+def test2(request):
+    return render(request, 'test2.html')
